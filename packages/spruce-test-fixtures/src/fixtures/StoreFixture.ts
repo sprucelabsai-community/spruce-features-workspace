58f2ae409fdb34a3aecd24139f66f44cfbcc1d6b
Taylor Romero <me@taylorrome.ro>
2021-05-28 09:15:08 -0600

    patch: getStoryFactory from store fixture

--- packages/spruce-test-fixtures/src/fixtures/StoreFixture.ts
+++ packages/spruce-test-fixtures/src/fixtures/StoreFixture.ts
@@ -6,20 +6,27 @@ import {
 } from '@sprucelabs/data-stores'
 
 export default class StoreFixture {
+	private storeFactory?: Promise<StoreFactory>
 	private loader?: Promise<StoreLoader>
 	private stores?: Promise<StoreFactory>
 
 	public async Store<N extends StoreName>(name: N): Promise<StoreMap[N]> {
-		if (!this.loader) {
-			this.loader = StoreLoader.getInstance()
-		}
-		const loader = await this.loader
+		const factory = await this.getStoreFactory()
+		return factory.Store(name)
+	}
 
+	public async getStoreFactory() {
 		if (!this.stores) {
-			this.stores = loader.loadStores()
+			if (!this.loader) {
+				this.loader = StoreLoader.getInstance()
+				await this.loader
+			}
+			const loader = await this.loader
+
+			this.storeFactory = loader.loadStores()
 		}
 
-		const stores = await this.stores
-		return stores.Store(name)
+		const factory = await this.storeFactory
+		return factory as StoreFactory
 	}
 }
