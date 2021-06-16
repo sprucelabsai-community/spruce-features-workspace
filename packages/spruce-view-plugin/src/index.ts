245dee86fa3c4b1a17ff0f11325520a2b7db6ff8
Tay <me@taylorrome.ro>
2021-06-16 16:44:01 -0600

    minor: AbstarctViewControllerTest

--- packages/spruce-view-plugin/src/index.ts
+++ packages/spruce-view-plugin/src/index.ts
@@ -1,3 +1,4 @@
 export { default as plugin } from './plugins/view.plugin'
 export * from './types/view.types'
 export { default as viewControllerUtil } from './utilities/viewController.utility'
+export { default as AbstractViewControllerTest } from './tests/AbstractViewControllerTest'
