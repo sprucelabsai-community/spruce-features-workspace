df9b051afa98ea2f9759f0983ed7c3896abd21a8
Tay <me@taylorrome.ro>
2021-07-14 12:44:11 -0600

    pathch: abststortest passes back up to beforeAll, beforeEach

--- packages/spruce-store-plugin/src/tests/AbstractStoreTest.ts
+++ packages/spruce-store-plugin/src/tests/AbstractStoreTest.ts
@@ -12,17 +12,8 @@ export default abstract class AbstractStoreTest extends AbstractSpruceFixtureTes
 	)
 	private static storeFixture: StoreFixture
 
-	protected static async beforeAll() {
-		await super.beforeAll()
-		await StoreFixture.beforeAll()
-	}
-
-	protected static async beforeEach() {
-		await StoreFixture.beforeEach()
-	}
-
 	protected static async connectToDatabase() {
-		return StoreFixture.DatabaseFixture().connectToDatabase()
+		return this.Fixture('database').connectToDatabase()
 	}
 
 	protected static async Store<N extends StoreName, O extends StoreOptions<N>>(
