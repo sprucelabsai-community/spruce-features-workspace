282d6564fadef127d696c885b9c1da82cd935ac3
Taylor Romero <me@taylorrome.ro>
2021-11-11 18:18:05 -0700

    location fixture

--- packages/spruce-test-fixtures/src/__tests__/behavioral/LocationFixture.test.ts
+++ packages/spruce-test-fixtures/src/__tests__/behavioral/LocationFixture.test.ts
@@ -1,18 +1,18 @@
 import { test, assert } from '@sprucelabs/test'
 import AbstractSpruceFixtureTest from '../../tests/AbstractSpruceFixtureTest'
-import { DEMO_NUMBER_MERCURY_FIXTURE } from '../../tests/constants'
+import { DEMO_NUMBER_LOCATION_FIXTURE } from '../../tests/constants'
 
 export default class LocationFixtureTest extends AbstractSpruceFixtureTest {
 	@test()
 	protected static async canCreateWithSpecificOrg() {
 		const org = await this.Fixture('organization').seedDemoOrganization({
 			name: 'Location fixture org',
-			phone: DEMO_NUMBER_MERCURY_FIXTURE,
+			phone: DEMO_NUMBER_LOCATION_FIXTURE,
 		})
 
 		const location = await this.Fixture('location').seedDemoLocation({
 			name: 'Location fixture location',
-			phone: DEMO_NUMBER_MERCURY_FIXTURE,
+			phone: DEMO_NUMBER_LOCATION_FIXTURE,
 			organizationId: org.id,
 		})
 
