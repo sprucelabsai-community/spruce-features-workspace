f94fb7a9f1a1688cb05c11353d191c0bd6d6e3d1
Taylor Romero <me@taylorrome.ro>
2021-12-11 17:03:36 -0700

    patch: type tweak

--- packages/spruce-test-fixtures/src/tests/eventMocker.ts
+++ packages/spruce-test-fixtures/src/tests/eventMocker.ts
@@ -1,16 +1,11 @@
-import {
-	EventContract,
-	EventNames,
-	SkillEventContract,
-} from '@sprucelabs/mercury-types'
+import { SkillEventContract } from '@sprucelabs/mercury-types'
 import { MercuryFixture } from '..'
 import SpruceError from '../errors/SpruceError'
 
 const eventMocker = {
-	/** @ts-ignore */
-	async makeEventThrow<Contract extends EventContract = SkillEventContract>(
+	async makeEventThrow(
 		mercuryFixture: MercuryFixture,
-		fqen: EventNames<Contract>
+		fqen: keyof SkillEventContract['eventSignatures']
 	) {
 		const client = await mercuryFixture.connectToApi()
 		await client.on(fqen as any, () => {
