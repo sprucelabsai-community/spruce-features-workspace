f9e7ab223906b7367bf6233b07fd821224e40f20
Taylor Romero <me@taylorrome.ro>
2022-08-29 14:09:03 -0600

    patch: import fix

--- packages/spruce-test-fixtures/src/__tests__/behavioral/faking/FakingAuthenticator.test.ts
+++ packages/spruce-test-fixtures/src/__tests__/behavioral/faking/FakingAuthenticator.test.ts
@@ -1,6 +1,6 @@
-import { fake } from '@sprucelabs/spruce-test-fixtures'
 import { test, assert } from '@sprucelabs/test'
 import AbstractSpruceFixtureTest from '../../../tests/AbstractSpruceFixtureTest'
+import fake from '../../../tests/decorators/fake'
 
 @fake.login()
 export default class FakingAuthenticatorTest extends AbstractSpruceFixtureTest {
